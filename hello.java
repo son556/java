519b3d0fd441725e544573e3e7a159f27943986f
son556 <shhmun02@naver.com>
2022-06-06 17:59:41 +0900

    2022-06-06 java start

---
+++ hello.java
@@ -0,0 +1,12 @@
+public class hello {
+    public static void main(String[] args){
+        System.out.println(3/2.5);
+        System.out.println("hello"); // 문자열은 반드시 ""으로 감싸야한다. 문자는 ''와 "" 둘다 가능
+        System.out.println("h");// 문자가 1개여도 ""으로 감싸면 문자열이 된다.
+        System.out.println("hello"+"world");
+        System.out.println('h' + 'e');
+        System.out.println("egoing said: \"Welcome program world\"");// "를 문자로 인식하기위해 \"를 사용
+        System.out.println("egoing said:\n Welcome\n program world"); // \n -> 줄바꿈
+
+    }
+}
