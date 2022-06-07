1009bb7cfe7075db2f5fd4672e31541e0276f4eb
son556 <96503318+son556@users.noreply.github.com>
2022-06-08 00:20:16 +0900

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,24 @@
+public class variable {
+    public static void main(String[] args){
+        int a; //정수 변수 선언
+        a = 1;
+        System.out.println(a + 1);
+        a = 2;
+        System.out.println(a + 2);
+        double b; // 실수 변수 선언
+        b = 1.1;
+        System.out.println(b + 2);
+
+        String first;
+        first = "coding";
+
+        String fir = "coding";
+        String c, d;
+        c = "coding";
+        d = "ererybody";
+        System.out.println(c + ' ' +d);
+    }
+}
+/**
+ 나는 나비가 되고 싶어
+ */
